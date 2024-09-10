4c32e2e2776367f1060136043f432a9ba4a1b6ce
Bruno Wunsch <bruno_wunsch@hotmail.com>
2024-09-09 21:56:44 -0300

    Created contexts and context provider

---
+++ providers/ContextProvider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+// import { NextUIProvider } from "@nextui-org/react";
+// import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
+
+import NavbarContextProvider from "@/context/NavbarContext";
+import ThemeContextProvider from "@/context/ThemeContext";
+
+const ContextProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    // <SessionProvider>
+      // <NextUIProvider>
+        <ThemeContextProvider>
+            <NavbarContextProvider>
+              <Toaster position="top-center" toastOptions={{duration: 3000}} />
+              {children}
+            </NavbarContextProvider>
+        </ThemeContextProvider>
+      // </NextUIProvider>
+    // </SessionProvider>
+  )
+};
+
+export default ContextProvider;
