72ca7cee4d847f8d0238344b89d6712e38f547d7
Bruno Wunsch <bruno_wunsch@hotmail.com>
2024-09-09 21:58:30 -0300

    Created default data for navbar

---
+++ lib/data.ts
@@ -0,0 +1,52 @@
+import { HiOutlineHome } from "react-icons/hi";
+import { BsDiagram3, BsBookmarks } from "react-icons/bs";
+import { AiOutlineTags } from "react-icons/ai";
+import { LiaUserShieldSolid } from "react-icons/lia";
+import { PiGearSix } from "react-icons/pi";
+import { FiUsers, FiLogOut } from "react-icons/fi";
+
+export const navbarInfo = [
+  {
+    label: "Dashboard",
+    url: "/",
+    type: "A",
+    icon: HiOutlineHome,
+  },
+  {
+    label: "Heat",
+    url: "/heat",
+    type: "A",
+    icon: BsDiagram3,
+  },
+  {
+    label: "Gas",
+    url: "/gas",
+    type: "A",
+    icon: AiOutlineTags,
+  },
+  {
+    label: "Movement",
+    url: "/movement",
+    type: "M",
+    icon: FiUsers,
+  },
+  {
+    label: "Users",
+    url: "/users",
+    type: "M",
+    icon: LiaUserShieldSolid,
+  },
+  {
+    label: "Settings",
+    url: "/settings",
+    type: "A",
+    icon: PiGearSix,
+  },
+  {
+    label: "Log Out",
+    url: "#",
+    type: "A",
+    icon: FiLogOut,
+    signOut: true,
+  },
+] as const;
