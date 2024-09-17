583655e570171a5175123d803629630662ff2ea6
Bruno Wunsch <bruno_wunsch@hotmail.com>
2024-09-16 21:28:35 -0300

    Removed color transition delay

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -3,7 +3,7 @@
 import { useNavbar } from "@/context/NavbarContext";
 import { cn } from "@/lib/utils";
 
-// import Header from "@/components/Header";
+import Header from "@/components/header/Header";
 import Navbar from "@/components/navbar/Navbar";
 
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
@@ -13,11 +13,11 @@ const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
     <>
       <Navbar isActive={isActive} />
       <main className={cn(
-        "flex flex-col gap-y-3 py-2 pl-[272px] pr-3 w-full h-[200%] transition-all bg-gray-50 dark:bg-neutral-900", 
+        "flex flex-col gap-y-3 py-2 pl-[272px] pr-3 w-full h-[200%] transition-all transition-color-none bg-gray-50 dark:bg-neutral-900 ", 
         isActive && "pl-[72px]"
         )}
       >
-        {/* <Header isActive={isActive} /> */}
+        <Header isActive={isActive} />
         <div className="flex flex-col gap-y-10 mx-2 md:mx-5">
           {children}
         </div>
