b0d46ebb267ee1a2b641b9a9f557a1b8e0de9772
Bruno Wunsch <bruno_wunsch@hotmail.com>
2024-09-16 21:27:44 -0300

    Added utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 };
+
+export function capitalize(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export function getInitials(fullName: string) {
+  const nameArray = fullName.split(" ");
+
+  const firstName = nameArray[0].charAt(0).toUpperCase();
+  const lastName = nameArray[nameArray.length - 1].charAt(0).toUpperCase();
+
+  return firstName + lastName;
+};
