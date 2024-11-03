951aa364d471767eb72456e9c5c167539d1bcf82
Bruno Wunsch <bruno_wunsch@hotmail.com>
2024-11-03 17:21:24 -0300

    Updated navbar options

--- lib/data.ts
+++ lib/data.ts
@@ -1,35 +1,74 @@
-import { HiOutlineHome } from "react-icons/hi";
-import { BsDiagram3 } from "react-icons/bs";
-import { AiOutlineTags } from "react-icons/ai";
-import { LiaUserShieldSolid } from "react-icons/lia";
-import { PiGearSix } from "react-icons/pi";
-import { FiUsers, FiLogOut } from "react-icons/fi";
+//? Home/dashboard
+import { VscGraph } from "react-icons/vsc"; // import { HiOutlineHome } from "react-icons/hi";
+
+//? Source table
+import { MdOutlineGraphicEq } from "react-icons/md";
+
+//? Tables
+import { LuCloudRainWind } from "react-icons/lu";
+import { LuWind } from "react-icons/lu";
+import { LuThermometerSun } from "react-icons/lu"; // import { BsThermometerHalf } from "react-icons/bs";
+import { LuSun } from "react-icons/lu";
+import { SiRainmeter } from "react-icons/si"; // import { WiHumidity } from "react-icons/wi";
+import { TbGauge } from "react-icons/tb"; // import { MdAtm } from "react-icons/md";
+
+//? Other options
+import { LiaUserShieldSolid } from "react-icons/lia"; // import { LuUser2 } from "react-icons/lu";
+import { LuSettings } from "react-icons/lu"; // import { PiGearSix } from "react-icons/pi";
+import { FiLogOut } from "react-icons/fi";
+
 
 export const navbarInfo = [
   {
     label: "Dashboard",
     url: "/",
     type: "A",
-    icon: HiOutlineHome,
+    icon: VscGraph,
   },
+
   {
-    label: "Heat",
-    url: "/heat",
+    label: "General",
+    url: "/general",
     type: "A",
-    icon: BsDiagram3,
+    icon: MdOutlineGraphicEq,
   },
   {
-    label: "Gas",
-    url: "/gas",
+    label: "Rain",
+    url: "/rain",
     type: "A",
-    icon: AiOutlineTags,
+    icon: LuCloudRainWind,
   },
   {
-    label: "Movement",
-    url: "/movement",
-    type: "M",
-    icon: FiUsers,
+    label: "Wind",
+    url: "/wind",
+    type: "A",
+    icon: LuWind,
   },
+  {
+    label: "Temperature",
+    url: "/temperature",
+    type: "A",
+    icon: LuThermometerSun,
+  },
+  {
+    label: "Light",
+    url: "/light",
+    type: "A",
+    icon: LuSun,
+  },
+  {
+    label: "Humidity",
+    url: "/humidity",
+    type: "A",
+    icon: SiRainmeter,
+  },
+  {
+    label: "Pressure",
+    url: "/pressure",
+    type: "A",
+    icon: TbGauge,
+  },
+
   {
     label: "Users",
     url: "/users",
@@ -40,7 +79,7 @@ export const navbarInfo = [
     label: "Settings",
     url: "/settings",
     type: "A",
-    icon: PiGearSix,
+    icon: LuSettings,
   },
   {
     label: "Log Out",
