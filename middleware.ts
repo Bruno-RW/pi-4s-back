783d4fed71b7122ecd396411cf5dfa9a3e0bc886
Bruno Wunsch <bruno_wunsch@hotmail.com>
2024-11-04 20:45:41 -0300

    Updated middleware routes

--- middleware.ts
+++ middleware.ts
@@ -7,19 +7,33 @@ interface RoutePermissions {
 
 const routePermissions: RoutePermissions = {
   "/api": null,
+
+  "/api/general": "A",
+  "/api/rain": "A",
+  "/api/wind": "A",
+  "/api/temperature": "A",
+  "/api/light": "A",
+  "/api/humidity": "A",
+  "/api/pressure": "A",
   "/api/users": "M",
-  "/api/clients": "M",
+
+  "/general": "A",
+  "/rain": "A",
+  "/wind": "A",
+  "/temperature": "A",
+  "/light": "A",
+  "/humidity": "A",
+  "/pressure": "A",
   "/users": "M",
-  "/clients": "M"
 };
 
 export default withAuth(
   function middleware(request: NextRequestWithAuth) {
     const path = Object.keys(routePermissions).find(key => request.nextUrl.pathname.startsWith(key));
 
-    if ( path && (!request.nextauth.token?.type || request.nextauth.token.type !== routePermissions[path]) ) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
+    // if ( path && (!request.nextauth.token?.type || request.nextauth.token.type !== routePermissions[path]) ) {
+    //   return NextResponse.redirect(new URL("/", request.url));
+    // }
 
     const origin = request.nextUrl.origin;
     const pathname = request.nextUrl.pathname;
