2f16440adc911ba4ed272fcd2cd454e313ea32c5
Bruno Wunsch <bruno_wunsch@hotmail.com>
2024-11-24 01:32:43 -0300

    Fixed text

--- components/ui/custom/ApiList.tsx
+++ components/ui/custom/ApiList.tsx
@@ -26,8 +26,8 @@ const ApiList: React.FC<ApiListProps> = ({ entityIdName, entityName, type = "adm
 
       {type === "admin" ? (
         <div className="flex flex-col gap-y-4">
-          <ApiAlert title="GET"    variant="public" description={`${baseUrl}/${entityName}`} />
-          <ApiAlert title="GET"    variant="public" description={`${baseUrl}/${entityName}/{${entityIdName}}`} />
+          <ApiAlert title="GET"    variant="admin" description={`${baseUrl}/${entityName}`} />
+          <ApiAlert title="GET"    variant="admin" description={`${baseUrl}/${entityName}/{${entityIdName}}`} />
           
           <ApiAlert title="PATCH"  variant="admin"  description={`${baseUrl}/${entityName}/{${entityIdName}}`} />
           <ApiAlert title="DELETE" variant="admin"  description={`${baseUrl}/${entityName}/{${entityIdName}}`} />
