26d726112f2403b61acba796dd763f25b37e6f98
Bruno Wunsch <bruno_wunsch@hotmail.com>
2024-12-01 15:56:11 -0300

    Commented unused routes

--- lib/data.ts
+++ lib/data.ts
@@ -5,12 +5,12 @@ import { VscGraph } from "react-icons/vsc"; // import { HiOutlineHome } from "re
 import { MdOutlineGraphicEq } from "react-icons/md";
 
 //? Tables
-import { LuCloudRainWind } from "react-icons/lu";
-import { LuWind } from "react-icons/lu";
-import { LuThermometerSun } from "react-icons/lu"; // import { BsThermometerHalf } from "react-icons/bs";
-import { LuSun } from "react-icons/lu";
-import { SiRainmeter } from "react-icons/si"; // import { WiHumidity } from "react-icons/wi";
-import { TbGauge } from "react-icons/tb"; // import { MdAtm } from "react-icons/md";
+// import { LuCloudRainWind } from "react-icons/lu";
+// import { LuWind } from "react-icons/lu";
+// import { LuThermometerSun } from "react-icons/lu"; // import { BsThermometerHalf } from "react-icons/bs";
+// import { LuSun } from "react-icons/lu";
+// import { SiRainmeter } from "react-icons/si"; // import { WiHumidity } from "react-icons/wi";
+// import { TbGauge } from "react-icons/tb"; // import { MdAtm } from "react-icons/md";
 
 //? Other options
 import { LiaUserShieldSolid } from "react-icons/lia"; // import { LuUser2 } from "react-icons/lu";
@@ -32,42 +32,42 @@ export const navbarInfo = [
     type: "A",
     icon: MdOutlineGraphicEq,
   },
-  {
-    label: "Rain",
-    url: "/rain",
-    type: "A",
-    icon: LuCloudRainWind,
-  },
-  {
-    label: "Wind",
-    url: "/wind",
-    type: "A",
-    icon: LuWind,
-  },
-  {
-    label: "Temperature",
-    url: "/temperature",
-    type: "A",
-    icon: LuThermometerSun,
-  },
-  {
-    label: "Light",
-    url: "/light",
-    type: "A",
-    icon: LuSun,
-  },
-  {
-    label: "Humidity",
-    url: "/humidity",
-    type: "A",
-    icon: SiRainmeter,
-  },
-  {
-    label: "Pressure",
-    url: "/pressure",
-    type: "A",
-    icon: TbGauge,
-  },
+  // {
+  //   label: "Rain",
+  //   url: "/rain",
+  //   type: "A",
+  //   icon: LuCloudRainWind,
+  // },
+  // {
+  //   label: "Wind",
+  //   url: "/wind",
+  //   type: "A",
+  //   icon: LuWind,
+  // },
+  // {
+  //   label: "Temperature",
+  //   url: "/temperature",
+  //   type: "A",
+  //   icon: LuThermometerSun,
+  // },
+  // {
+  //   label: "Light",
+  //   url: "/light",
+  //   type: "A",
+  //   icon: LuSun,
+  // },
+  // {
+  //   label: "Humidity",
+  //   url: "/humidity",
+  //   type: "A",
+  //   icon: SiRainmeter,
+  // },
+  // {
+  //   label: "Pressure",
+  //   url: "/pressure",
+  //   type: "A",
+  //   icon: TbGauge,
+  // },
 
   {
     label: "Users",
