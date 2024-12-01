2055f5778fd922eb57f12538608b821febba5b3b
Bruno Wunsch <bruno_wunsch@hotmail.com>
2024-12-01 16:55:20 -0300

    Created chart integration

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,36 +1,48 @@
-"use client";
-
-import { useEffect } from "react";
-import toast from "react-hot-toast";
-
-import ColumnChart from "@/components/charts/ColumnChart";
-import BarChart from "@/components/charts/BarChart";
-
 import db from "@/lib/db";
+import { formatDateTime } from "@/lib/utils";
+import { avgRainLvlByMonth } from "@/lib/chartData";
+
+import Chart from "@/components/charts/Chart";
 
 const HomePage = async() => {
-  useEffect(() => toast.remove(), []);
+  const data = await db.nit2xli.findMany({take: 100 });
+
+  const formattedData = data.map(item => ({
+    ...item,
+    time: formatDateTime(item.time, false, { month: "short" })
+  }));
+
+  const avgRainLvl = avgRainLvlByMonth(formattedData);
 
   return (
-  <div className="flex flex-col">
+    <div className="flex flex-col">
+      <div className="flex flex-row">
+        <Chart 
+          cardTitle="Rain Level"
+          cardDescription="Average rain level by month (mm)"
+          chartType="column"
+          data={avgRainLvl}
+          xColumn="time"
+          yColumn="_avg_emw_rain_lvl"
+        />
 
-    {/* <div className="flex flex-row">
+        <Chart 
+          cardTitle="Temperature"
+          cardDescription="Average temperature by month (ºC)"
+          chartType="bar"
+          data={avgRainLvl}
+          xColumn="time"
+          yColumn="_avg_emw_rain_lvl"
+        />
+
+        {/* <Chart 
+          cardTitle="Humidity"
+          chartType="bar"
+          xColumn="time"
+          yColumn="emw_avg_wind_speed"
+        /> */}
+      </div>
     </div>
-    */}
-
-    <div className="flex-1 p-2">
-      <ColumnChart className="" />
-    </div> 
-
-    <div className="flex-1 p-2">
-      <BarChart 
-        cardTitle="Uva"
-        chartData={['']}
-        className=""
-      />
-    </div> 
-
-  </div>
   );
 };
 
